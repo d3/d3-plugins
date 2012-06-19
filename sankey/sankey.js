ef3dcb794622e0de28ccb0e5460eb7fa63732e41
Nathan Malkin <nmalkin@mozilla.com>
2012-06-19 11:11:10 -0700

    Fix Sankey plugin requiring global "width" variable
    When computing the node breadths, the Sankey plugin looked for the width
    of the graph in the variable "width" (not declared anywhere in the
    plugin; therefore, it must exist globally), rather than in its internal
    property "size", where the width is stored at construction-time.

--- sankey/sankey.js
+++ sankey/sankey.js
@@ -127,7 +127,7 @@ d3.sankey = function() {
 
     //
     moveSinksRight(x);
-    scaleNodeBreadths((width - nodeWidth) / (x - 1));
+    scaleNodeBreadths((size[0] - nodeWidth) / (x - 1));
   }
 
   function moveSourcesRight() {

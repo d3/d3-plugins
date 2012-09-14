9cd400685613aaa5877087654958a6af9cf2ae87
Jason Davies <jason@jasondavies.com>
2012-09-14 19:27:51 +0100

    Fix parallels projections with no inverse.

--- geo/projection/projection.js
+++ geo/projection/projection.js
@@ -392,15 +392,15 @@
   function singleParallelProjection(forwardAt, inverseAt) {
     var φ0 = 0,
         forward = forwardAt(φ0),
-        inverse = inverseAt(φ0),
-        p = inverse
+        inverse = inverseAt ? inverseAt(φ0) : null,
+        p = inverseAt
             ? projection(function() { return forward(λ, φ); }, function(x, y) { return inverse(x, y); })
             : projection(function() { return forward(λ, φ); });
 
     p.parallel = function(_) {
       if (!arguments.length) return φ0 / π * 180;
       forward = forwardAt(φ0 = _ * π / 180);
-      inverse = inverseAt(φ0);
+      if (inverseAt) inverse = inverseAt(φ0);
       return p;
     };
 
@@ -411,15 +411,15 @@
     var φ0 = 0,
         φ1 = π / 3,
         project = forwardAt(φ0, φ1),
-        inverse = inverseAt(φ0, φ1),
-        p = inverse
+        inverse = inverseAt ? inverseAt(φ0, φ1) : null,
+        p = inverseAt
             ? projection(function() { return forward(λ, φ); }, function(x, y) { return inverse(x, y); })
             : projection(function() { return forward(λ, φ); });
 
     p.parallels = function(_) {
       if (!arguments.length) return [φ0 / π * 180, φ1 / π * 180];
       forward = forwardAt(φ0 = _ * π / 180, φ1 = _ * π / 180);
-      inverse = inverseAt(φ0, φ1);
+      if (inverseAt) inverse = inverseAt(φ0, φ1);
       return p;
     };
 

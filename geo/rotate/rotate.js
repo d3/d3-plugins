98052251a494dca9c43cf1680fd1746ddf3135d3
Jason Davies <jason@jasondavies.com>
2012-09-14 22:28:16 +0100

    Add rotate.invert.

--- geo/rotate/rotate.js
+++ geo/rotate/rotate.js
@@ -1,12 +1,13 @@
 (function() {
+  var radians = Math.PI / 180;
+
   d3.geo.rotate = function(z, y, x) {
     if (!(x = +x || 0) && !(y = +y || 0) && !(z = +z || 0)) {
       return identity;
     }
 
     var m = matrix(y, x),
-        zAngle = z,
-        radians = Math.PI / 180;
+        zAngle = z;
 
     function rotate(coordinates) {
       var λ = coordinates[0] * radians + zAngle,
@@ -28,6 +29,25 @@
     }
 
     return rotate;
+  };
+
+  rotate.invert = function(coordinates) {
+    var λ = coordinates[0] * d3_geo_radians,
+        φ = coordinates[1] * d3_geo_radians,
+        cosφ = Math.cos(φ),
+        x = Math.cos(λ) * cosφ,
+        y = Math.sin(λ) * cosφ,
+        z = Math.sin(φ),
+        mx = m[0],
+        my = m[1],
+        mz = m[2],
+        rx = x * mx[0] + y * my[0] + z * mz[0],
+        ry = x * mx[1] + y * my[1] + z * mz[1],
+        rz = x * mx[2] + y * my[2] + z * mz[2];
+    return [
+      Math.atan2(ry, rx) / radians - zAngle,
+      Math.asin(rz) / radians
+    ];
   };
 
   function matrix(y, x) {

c8709d00e291e5bd9953796b086ee966486baa50
Jason Davies <jason@jasondavies.com>
2012-09-24 14:09:33 +0100

    Reverse direction of δγ for rotations.
    Clockwise angle from N seems more intuitive.

--- geo/projection/projection.js
+++ geo/projection/projection.js
@@ -810,8 +810,8 @@
           z = Math.sin(φ),
           k = z * cosδφ + x * sinδφ;
       return [
-        Math.atan2(y * cosδγ - k * sinδγ, x * cosδφ - z * sinδφ),
-        Math.asin(Math.max(-1, Math.min(1, k * cosδγ + y * sinδγ)))
+        Math.atan2(y * cosδγ + k * sinδγ, x * cosδφ - z * sinδφ),
+        Math.asin(Math.max(-1, Math.min(1, k * cosδγ - y * sinδγ)))
       ];
     }
 
@@ -820,9 +820,9 @@
           x = Math.cos(λ) * cosφ,
           y = Math.sin(λ) * cosφ,
           z = Math.sin(φ),
-          k = z * cosδγ - y * sinδγ;
+          k = z * cosδγ + y * sinδγ;
       return [
-        Math.atan2(y * cosδγ + z * sinδγ, x * cosδφ + k * sinδφ),
+        Math.atan2(y * cosδγ - z * sinδγ, x * cosδφ + k * sinδφ),
         Math.asin(Math.max(-1, Math.min(1, k * cosδφ - x * sinδφ)))
       ];
     };

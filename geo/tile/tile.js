0690dd51585abce6abab43a44ed269e4783e981d
Mike Bostock <mbostock@gmail.com>
2012-11-27 10:19:03 -0800

    Add tile.zoomDelta.

--- geo/tile/tile.js
+++ geo/tile/tile.js
@@ -1,11 +1,12 @@
 d3.geo.tile = function() {
   var size = [960, 500],
       scale = 256,
-      translate = [size[0] / 2, size[1] / 2];
+      translate = [size[0] / 2, size[1] / 2],
+      zoomDelta = 0;
 
   function tile() {
     var z = Math.max(Math.log(scale) / Math.LN2 - 8, 0),
-        z0 = Math.round(z),
+        z0 = Math.round(z + zoomDelta),
         k = Math.pow(2, z - z0 + 8),
         origin = [(translate[0] - scale / 2) / k, (translate[1] - scale / 2) / k],
         tiles = [],
@@ -39,6 +40,12 @@ d3.geo.tile = function() {
   tile.translate = function(_) {
     if (!arguments.length) return translate;
     translate = _;
+    return tile;
+  };
+
+  tile.zoomDelta = function(_) {
+    if (!arguments.length) return zoomDelta;
+    zoomDelta = +_;
     return tile;
   };
 

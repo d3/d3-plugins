ed111f9a92fd8dc92ed50ade468b42efb619694f
Mike Bostock <mbostock@gmail.com>
2013-01-07 20:40:21 -0800

    Add Boggs eumorphic.

--- geo/projection/projection.js
+++ geo/projection/projection.js
@@ -574,6 +574,15 @@
     ];
   };
 
+  function boggs(λ, φ) {
+    var s = sinusoidal(λ, φ),
+        m = mollweide(λ, φ);
+    return [
+      (s[0] + m[0]) / 2,
+      (s[1] + m[1]) / 2
+    ];
+  }
+
   function homolosine(λ, φ) {
     return Math.abs(φ) > sinumollφ
         ? (λ = mollweide(λ, φ), λ[1] -= φ > 0 ? sinumolly : -sinumolly, λ)
@@ -1492,6 +1501,7 @@
   (d3.geo.august = function() { return projection(august); }).raw = august;
   (d3.geo.baker = function() { return projection(baker); }).raw = baker;
   (d3.geo.berghaus = berghausProjection).raw = berghaus;
+  (d3.geo.boggs = function() { return projection(boggs); }).raw = boggs;
   (d3.geo.bonne = function() { return singleParallelProjection(bonne).parallel(45); }).raw = bonne;
   (d3.geo.collignon = function() { return projection(collignon); }).raw = collignon;
   (d3.geo.conicConformal = function() { return doubleParallelProjection(conicConformal); }).raw = conicConformal;

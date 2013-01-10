096f331a25e995ffe41399671fe3990589ce8e80
Jason Davies <jason@jasondavies.com>
2013-01-10 13:43:25 +0000

    Add littrow.invert.

--- geo/projection/littrow.js
+++ geo/projection/littrow.js
@@ -5,4 +5,17 @@ function littrow(λ, φ) {
   ];
 }
 
+littrow.invert = function(x, y) {
+  var x2 = x * x,
+      y2 = y * y,
+      y2_1 = y2 + 1,
+      cosφ = x
+        ? Math.SQRT1_2 * Math.sqrt((y2_1 - Math.sqrt(x2 * x2 + 2 * x2 * (y2 - 1) + y2_1 * y2_1)) / x2 + 1)
+        : 1 / Math.sqrt(y2_1);
+  return [
+    asin(x * cosφ),
+    sgn(y) * acos(cosφ)
+  ];
+};
+
 (d3.geo.littrow = function() { return projection(littrow); }).raw = littrow;

5a20e6dab1423fa5dd78e2b1a7247296df67db69
Jason Davies <jason@jasondavies.com>
2013-02-07 10:41:00 +0000

    Code formatting.

--- geo/projection/fahey.js
+++ geo/projection/fahey.js
@@ -1,19 +1,19 @@
 function fahey(λ, φ) {
   var t = Math.tan(φ / 2);
   return [
-    λ * fahey_k * asqrt(1 - t * t),
-    (1 + fahey_k) * t
+    λ * faheyK * asqrt(1 - t * t),
+    (1 + faheyK) * t
   ];
 }
 
 fahey.invert = function(x, y) {
-  var t = y / (1 + fahey_k);
+  var t = y / (1 + faheyK);
   return [
-    x ? x / (fahey_k * asqrt(1 - t * t)) : 0,
+    x ? x / (faheyK * asqrt(1 - t * t)) : 0,
     2 * Math.atan(t)
   ];
 };
 
-var fahey_k = Math.cos(35 * radians);
+var faheyK = Math.cos(35 * radians);
 
 (d3.geo.fahey = function() { return projection(fahey); }).raw = fahey;

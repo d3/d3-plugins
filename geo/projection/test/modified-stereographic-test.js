6490412b5bee1d868450880beb9e0eecaec1e578
Jason Davies <jason@jasondavies.com>
2013-02-09 23:38:36 +0000

    Add tests for d3.geo.modifiedStereographic.

---
+++ geo/projection/test/modified-stereographic-test.js
@@ -0,0 +1,24 @@
+require("./env");
+
+var vows = require("vows"),
+    assert = require("assert");
+
+var suite = vows.describe("d3.geo.modifiedStereographic");
+
+suite.addBatch({
+  "modifiedStereographic": {
+    topic: d3.geo.modifiedStereographic,
+    "projections and inverse projections": function(modifiedStereographic) {
+      assert.equalInverse(modifiedStereographic, [  0,   0], [480,        250]);
+      assert.equalInverse(modifiedStereographic, [  0, -45], [480,        363.225114]);
+      assert.equalInverse(modifiedStereographic, [  0,  45], [480,        136.774885]);
+      assert.equalInverse(modifiedStereographic, [-90,   0], [179.334000, 250]);
+      assert.equalInverse(modifiedStereographic, [ 90,   0], [780.666000, 250]);
+      assert.equalInverse(modifiedStereographic, [-80,  15], [244.261692, 178.493228]);
+      assert.equalInverse(modifiedStereographic, [  1,   1], [482.420181, 247.579387]);
+      assert.equalInverse(modifiedStereographic, [ 15,  45], [508.846677, 134.854913]);
+    }
+  }
+});
+
+suite.export(module);

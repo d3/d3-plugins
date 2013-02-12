8f741d897909b42631ae9e2f6aeb71da9e8606d7
Jason Davies <jason@jasondavies.com>
2013-02-12 09:17:54 +0000

    Simplify.

--- geo/projection/lagrange.js
+++ geo/projection/lagrange.js
@@ -16,11 +16,10 @@ function lagrange(n) {
     if (y0 > 2) return null;
 
     x /= 2, y /= 2;
-    var x2 = x * x, y2 = y * y,
-        nλ = Math.atan2(2 * x, 1 - x2 - y2),
+    var y2 = y * y,
+        nλ = Math.atan2(2 * x, 1 - x * x - y2),
         cosnλ = Math.cos(nλ),
-        v = (Math.sqrt(y2 * (cosnλ * cosnλ - 1) + 1) + cosnλ * y) / (1 - y),
-        v2n = Math.pow(v, 2 / n);
+        v2n = Math.pow((Math.sqrt(y2 * (cosnλ * cosnλ - 1) + 1) + cosnλ * y) / (1 - y), 2 / n);
     return [
       nλ / n,
       asin((v2n - 1) / (v2n + 1))

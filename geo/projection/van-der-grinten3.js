b140234b98a91065ec3aa97a093a9e7345604153
Jason Davies <jason@jasondavies.com>
2013-02-14 09:41:16 +0000

    Add d3.geo.vanDerGrinten3.invert.

--- geo/projection/van-der-grinten3.js
+++ geo/projection/van-der-grinten3.js
@@ -3,13 +3,22 @@ function vanDerGrinten3(λ, φ) {
   var sinθ = Math.abs(2 * φ / π),
       θ = asin(sinθ);
   if (Math.abs(λ) < ε || Math.abs(Math.abs(φ) - π / 2) < ε) return [0, sgn(φ) * π * Math.tan(θ / 2)];
-  var cosθ = Math.cos(θ),
-      A = Math.abs(π / λ - λ / π) / 2,
-      y1 = sinθ / (1 + cosθ);
+  var A = Math.abs(π / λ - λ / π) / 2,
+      y1 = sinθ / (1 + Math.cos(θ));
   return [
     sgn(λ) * π * (asqrt(A * A + 1 - y1 * y1) - A),
     sgn(φ) * π * y1
   ];
 }
+
+vanDerGrinten3.invert = function(x, y) {
+  if (!y) return [x, 0];
+  var y1 = y / π,
+      A = (π * π * (1 - y1 * y1) - x * x) / (2 * π * x);
+  return [
+    x ? π * (sgn(x) * Math.sqrt(A * A + 1) - A) : 0,
+    π / 2 * Math.sin(2 * Math.atan(y1))
+  ];
+};
 
 (d3.geo.vanDerGrinten3 = function() { return projection(vanDerGrinten3); }).raw = vanDerGrinten3;

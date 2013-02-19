0bd760c11b4c6b243777f3c168f5904d3ca205fe
Jason Davies <jason@jasondavies.com>
2013-02-19 23:56:25 +0000

    Add d3.geo.vanDerGrinten4.invert.

--- geo/projection/test/van-der-grinten4-test.js
+++ geo/projection/test/van-der-grinten4-test.js
@@ -10,6 +10,17 @@ suite.addBatch({
     topic: d3.geo.vanDerGrinten4,
     "doesn't generate NaN": function(vanDerGrinten4) {
       assert.inDelta(vanDerGrinten4([20, 1e-7]), [532.359877, 250], 1e-6);
+    },
+    "projections and inverse projections": function(vanDerGrinten4) {
+      assert.equalInverse(vanDerGrinten4, [  0,   0], [480,        250]);
+      assert.equalInverse(vanDerGrinten4, [  0, -45], [480,        367.809724]);
+      assert.equalInverse(vanDerGrinten4, [  0,  45], [480,        132.190275]);
+      assert.equalInverse(vanDerGrinten4, [-90,   0], [244.380550, 250]);
+      assert.equalInverse(vanDerGrinten4, [ 90,   0], [715.619449, 250]);
+      assert.equalInverse(vanDerGrinten4, [-80,  15], [274.023642, 209.610498]);
+      assert.equalInverse(vanDerGrinten4, [  1,   1], [482.617670, 247.382005]);
+      assert.equalInverse(vanDerGrinten4, [ 15,  45], [509.605158, 131.892658]);
+      assert.equalInverse(vanDerGrinten4, [-180, 15], [ 12.226093, 204.955105]);
     }
   }
 });
